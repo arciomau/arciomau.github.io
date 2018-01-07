0bfe915ed5ad3cb4abcd42025cfe8828ad3ab12f
arciomau <arciomau@gmail.com>
2018-01-08 01:43:11 +0300

    unmin css, date modification

---
+++ js/days.js
@@ -0,0 +1,30 @@
+function getdays(dayofupd) {
+    var arr = (dayofupd+'').split('.',3);
+    var day = parseInt(arr[0],10), month = parseInt(arr[1],10), year = parseInt(arr[2],10);
+    month--;
+    var now = new Date().getTime();
+    var updday = new Date(year,month,day).getTime();
+    var diff = Math.floor((now-updday)/(1000*3600*24));
+    var ten = parseInt((diff.toString()).slice(-2),10);
+    var result = undefined;
+    switch (diff) {
+        case 0: return result = 'сёння';
+        case 1: return result = 'учора';
+        case 2: return result = 'заўчора';
+        default: {
+            function noun (ten) {
+                if (parseInt((ten+'').slice(-1),10)===1) {return diff +' дзень'}
+                else if (parseInt((ten+'').slice(-2,-1),10)>1 &&
+                (parseInt((ten+'').slice(-1),10)<5)) {return diff +' дні'}
+                else {return diff+' дзён'}
+            }
+            result = noun(ten) + ' таму.';
+            if (diff>31) {result = result+' Пакуль нічога не пішу.'}
+        }
+    }
+    return result;
+}
+
+function writediff(day,month,year) {
+    document.getElementById("update").innerHTML = getdays(day,month,year);
+}
