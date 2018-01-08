35f505d571632280e99146e52d9ea8569cfaf2be
arciomau <arciomau@gmail.com>
2018-01-08 15:10:58 +0300

    fix days.js

--- js/days.js
+++ js/days.js
@@ -1,22 +1,24 @@
 function getdays(dayofupd) {
     var arr = (dayofupd+'').split('.',3);
     var day = parseInt(arr[0],10), month = parseInt(arr[1],10), year = parseInt(arr[2],10);
+    arr = undefined;
     month--;
     var now = new Date().getTime();
     var updday = new Date(year,month,day).getTime();
     var diff = Math.floor((now-updday)/(1000*3600*24));
-    var ten = parseInt((diff+'').slice(-2),10);
-    var result = undefined;
+    var ten = (diff+'').slice(-2);
+    var result;
     switch (diff) {
         case 0: return result = 'сёння.';
         case 1: return result = 'учора.';
         case 2: return result = 'заўчора.';
         default: {
             function noun (ten) {
-                if (parseInt((ten+'').slice(-1),10)===1) {return diff +' дзень'}
-                else if (parseInt((ten+'').slice(-2,-1),10)>1 &&
-                parseInt((ten+'').slice(-1),10)>1 &&
-                parseInt((ten+'').slice(-1),10)<5) {return diff +' дні'}
+                if (ten.slice(-1)=='1' &&
+                ten.slice(-2,-1)!='1') {return diff +' дзень'}
+                else if (ten.slice(-2,-1)!='1'&&
+                (ten.slice(-1)=='2' || ten.slice(-1)=='2' ||
+                ten.slice(-1)=='3' || ten.slice(-1)=='4')){return diff +' дні'}
                 else {return diff+' дзён'}
             }
             result = noun(ten) + ' таму.';
